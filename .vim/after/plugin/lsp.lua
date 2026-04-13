30983f1dfba6dd7dfba26141d67c21a3640d8b06
Nikita Kouevda <nkouevda@gmail.com>
2026-04-13 14:06:40 -0700

    nvim: s/type_definition/definition/g

--- .vim/after/plugin/lsp.lua
+++ .vim/after/plugin/lsp.lua
@@ -1,6 +1,6 @@
 -- LSP
-vim.keymap.set('n', 'gd', vim.lsp.buf.type_definition)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', 'g\\', function()
   vim.cmd('split')
-  vim.lsp.buf.type_definition()
+  vim.lsp.buf.definition()
 end)
